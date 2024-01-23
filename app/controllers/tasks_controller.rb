2ecfca3260d79fcae94f9ed071b47e89ec2f4120
Ruth Iruoaboroghene Felix <ruthflex@yahoo.co.uk>
2024-01-23 21:28:12 +0100

    commit task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    @completed_status = @task.completed? ? 'completed' : 'incomplete'
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to task_path(task)
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :complete)
+  end
+end
